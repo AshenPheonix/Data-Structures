9dd7247eb5a22557bd1f51c2aed5a3d1095d6b20
AshenPheonix <brendlorend@gmail.com>
2019-10-14 12:17:50 -0700

    wip: stack done

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -66,6 +66,8 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
+        if self.head == None:
+            return None
         temp=self.head.value
         self.delete(self.head)
         return temp
@@ -88,7 +90,9 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        temp=self.head.value
+        if self.tail == None:
+            return None
+        temp=self.tail.value
         self.delete(self.tail)
         return temp
 
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,26 @@
-from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
+        # Allows for easier inserts and deletes when we don't know how large the object will be
         # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.size+=1
+        self.storage.add_to_head(value)
 
     def pop(self):
-        pass
+        if self.size < 1:
+            return None
+        else:
+            self.size-=1
+            temp = self.storage.remove_from_head()
+            return temp
 
     def len(self):
-        pass
+        return self.size
