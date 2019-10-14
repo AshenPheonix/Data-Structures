017d246a82873a22bf5621b77b4a542f30c78982
AshenPheonix <brendlorend@gmail.com>
2019-10-14 10:29:16 -0700

    WIP: DLL Adds done

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -52,7 +52,15 @@ class DoublyLinkedList:
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value,None,self.head)
+
+        if self.head != None:
+            self.head.prev=new_node
+        else:
+            self.tail=new_node
+
+        self.head=new_node
+        self.length += 1
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
@@ -60,11 +68,19 @@ class DoublyLinkedList:
     def remove_from_head(self):
         pass
 
-   """Wraps the given value in a ListNode and inserts it 
+    """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        pass
+        if self.tail==None:
+            self.add_to_head(value)
+        else:
+            new_node = ListNode(value,self.tail,None)
+            
+            self.tail.next=new_node
+            self.tail=new_node
+            
+            self.length+=1
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
